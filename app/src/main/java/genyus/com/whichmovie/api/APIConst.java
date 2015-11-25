36a301b94682fca1154f70646a9d7aecfef28dc5
marshallino16 <anthonyfernandez16@gmail.com>
2015-11-26 00:11:24 +0100

    add api path

--- app/src/main/java/genyus/com/whichmovie/api/APIConst.java
+++ app/src/main/java/genyus/com/whichmovie/api/APIConst.java
@@ -8,4 +8,5 @@ public interface APIConst {
     public final static String API_TOKEN = "c1e4b32ab37e5086fe5c09521c0e67a7";
     public final static String API_BASE_URL = "http://api.themoviedb.org/3/";
     public final static String API_LIST_CATEGORIES = "http://api.themoviedb.org/3/genre/movie/list";
+    public final static String API_LIST_MOVIES_CATEGORY = "http://api.themoviedb.org/3/genre/[CAT]/movies";
 }
