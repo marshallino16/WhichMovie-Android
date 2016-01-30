2d982db6470ebdc29b5756f434f96ee7a7f238a4
marshallino16 <marshallino16@gmail.com>
2016-01-30 19:38:26 +0100

    add youtube player API

---
+++ app/src/main/java/genyus/com/whichmovie/classes/Config.java
@@ -0,0 +1,11 @@
+package genyus.com.whichmovie.classes;
+
+public final class Config {
+
+    private Config() {
+    }
+
+    public static final String YOUTUBE_API_KEY_DEBUG = "AIzaSyBNUilQC42HZktNKhsK1sjzGwu_nm01qXc";
+    public static final String YOUTUBE_API_KEY_RELEASE = "AIzaSyBS04zKv4eH2pF18lOhBOxz3DELN3VLJg0";
+
+}
