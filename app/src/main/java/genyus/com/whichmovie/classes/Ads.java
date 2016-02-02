8ac031fb05ed543413b92037f35193587b8e015e
marshallino16 <marshallino16@gmail.com>
2016-02-02 16:38:01 +0100

    ads play services libraries

---
+++ app/src/main/java/genyus/com/whichmovie/classes/Ads.java
@@ -0,0 +1,11 @@
+package genyus.com.whichmovie.classes;
+
+/**
+ * Created by GENyUS on 02/02/16.
+ */
+public class Ads {
+
+    public final static String inter_image = "ca-app-pub-8173266224774166/7832831538";
+    public final static String inter_video = "ca-app-pub-8173266224774166/1786297938";
+
+}
