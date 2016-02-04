84421a21cb62b64d91cd03b800d0850dcd46ef0c
marshallino16 <marshallino16@gmail.com>
2016-02-04 16:30:14 +0100

    add favorite movie activity

---
+++ app/src/main/java/genyus/com/whichmovie/FavoriteMovieActivity.java
@@ -0,0 +1,18 @@
+package genyus.com.whichmovie;
+
+import android.support.v7.app.AppCompatActivity;
+
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.EActivity;
+
+/**
+ * Created by GENyUS on 04/02/16.
+ */
+@EActivity(R.layout.activity_favorite_movie)
+public class FavoriteMovieActivity extends AppCompatActivity {
+
+
+    @AfterViews
+    protected void afterView() {
+    }
+}
