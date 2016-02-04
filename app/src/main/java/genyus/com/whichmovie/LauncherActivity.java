d2fcb9addbc9e2f5f50aeacc102501cbb1b33641
marshallino16 <marshallino16@gmail.com>
2016-02-04 16:01:54 +0100

    add launcher onboarding first screen

---
+++ app/src/main/java/genyus/com/whichmovie/LauncherActivity.java
@@ -0,0 +1,18 @@
+package genyus.com.whichmovie;
+
+import android.support.v7.app.AppCompatActivity;
+
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.EActivity;
+
+/**
+ * Created by GENyUS on 04/02/16.
+ */
+@EActivity(R.layout.fragment_first_screen)
+public class LauncherActivity extends AppCompatActivity {
+
+    @AfterViews
+    protected void afterView(){
+
+    }
+}
